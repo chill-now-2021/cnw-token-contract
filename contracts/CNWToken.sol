2dc09efef52bb7f42431a6526a6cfc340a47fb6a
innocentguy <yan.ntusc@gmail.com>
2021-03-06 13:48:19 +0800

    update token name

--- contracts/CNWToken.sol
+++ contracts/CNWToken.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.6.0 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract CNWToken is ERC20 {
-  constructor() ERC20('CNW', 'CNW') public {
+  constructor() ERC20('ChillNow', 'CNW') public {
     _setupDecimals(6);
-    _mint(msg.sender, 10**10*10**18);
+    _mint(msg.sender, 10**10*10**6);
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -68,16 +68,17 @@ module.exports = {
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       // confirmations: 2, // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
     mainnet: {
       provider: () =>
         new HDWalletProvider(
           privatekey,
-          `https://ropsten.infura.io/v3/${infuraProjectId}`
+          `https://mainnet.infura.io/v3/${infuraProjectId}`
         ),
       network_id: 1, // Mainnet's id
-      gas: 5500000,
+      // gas: 1000000,
+      // gasPrice: 150 * 10 ** 9,
     },
     // Useful for private networks
     // private: {
